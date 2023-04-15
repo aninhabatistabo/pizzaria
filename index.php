e6f24ab8ab597c0923a0c6cb0fc5ea91374f74a8
aninhabatistabo <126928646+aninhabatistabo@users.noreply.github.com>
2023-04-14 22:03:12 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <!-- Meta tags Obrigatórias -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+	<title>Pizzaria Sua Fome Minha Alegria</title>
+	
+  </head>
+  <body>
+    <header class="text white d-flex">
+      <div class="container d-flex justify-content-between align-items-center">
+	     <img src="img/logo.png" alt="Logo Pizzaria" />
+
+<nav>
+  <ul class="menu list-inline">
+    <li class="list-inline-item"><a href='#'>Home</a></li>
+    <li class="list-inline-item"><a href='#'>Sobre</a></li>
+    <li class="list-inline-item"><a href='#'>Cardapio</a></li>
+    <li class="list-inline-item"><a href='#'>Contato</a></li>
+    
+</ul>
+</nav>
+
+</div>
+
+	</header>
+  <section class="container">
+    <div class="row">
+      <nav class="col-md-2"
+      <ul class-"list-unstyled">
+        <li><a href"#">Home</a></li>
+        <li><a href"q1.html">Questao 1</a></li>
+        <li><a href"q2.html">Questao 2</a></li>
+        <li><a href"q3.html">Questao 3</a></li>
+        <li><a href"q4.html">Questao 4</a></li>
+
+     </ul>
+     </nav>
+<main class="col-md-9"></main>
+    <h1>Home</h1>
+    <p>
+
+    Mussum Ipsum, <strong>cacilds vidis litro abertis</strong>. Copo furadis é disculpa de bebadis, arcu quam euismod magna.Sapien in monti palavris qui num significa nadis i pareci latim.Detraxit consequat et quo num tendi nada.Admodum accumsan disputationi eu sit. Vide electram sadipscing et per.
+
+    </p>
+</main>
+</div>
+</section>
+<footer class="text-white">
+  <section class="container">
+    <div class="row">
+      <ul class="col-sm-6">
+         <li><h6>Links</h6><li>
+         <li>Home</li>
+         <li>Cardapio</li>
+         <li>Contato</li>
+
+      </ul>
+
+      <ul class="col-sm-6">
+         <li><h6>Pizzas</h6><li>
+         <li>Tradicionais</li>
+         <li>Nobres</li>
+         <li>Doces</li>
+
+</ul>
+</div>
+</section>
+
+
+    <!-- JavaScript (Opcional) -->
+    <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
